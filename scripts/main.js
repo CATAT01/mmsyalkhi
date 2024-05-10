fb50325bab77ab3da7851ea39f71598530b5a323
CATAT01 <168577967+CATAT01@users.noreply.github.com>
2024-05-10 22:22:34 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,9 @@ $('a.smooth-scroll')
     }
   }
 });
+// Clear form before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
